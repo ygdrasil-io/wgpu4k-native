5ca5752dc5c7a0a69b768317c86c222ca96d670b
ygdrasil-io <alexandre.mommers@gmail.com>
2024-10-20 16:00:09 +0200

    Enhance Buffer class with comprehensive read/write functions
    This commit adds extensive read and write methods for various data types to the Buffer class. These include support for primitives such as Byte, UByte, Char, Short, Int, Long, Float, and Double, as well as arrays of these types. This ensures more detailed and flexible manipulation of the Buffer's data contents.

--- wgpu4k-native/src/commonMain/kotlin/ffi/Buffer.kt
+++ wgpu4k-native/src/commonMain/kotlin/ffi/Buffer.kt
@@ -1,17 +1,79 @@
 package ffi
 
+@OptIn(ExperimentalUnsignedTypes::class)
 expect class Buffer(handler: NativeAddress, size: ULong) {
-
     val size: ULong
     val handler: NativeAddress
 
-    fun readLong(): Long
-    fun writeLong(value: Long)
-    fun readFloat(): Float
-    fun writeFloat(value: Float)
-    fun readInt(): Int
-    fun writeInt(value: Int)
-    fun readPointer(): NativeAddress
-    fun writePointer(value: NativeAddress)
+    fun writeByte(value: Byte, offset: ULong = 0uL)
+    fun readByte(offset: ULong = 0uL): Byte
 
+    fun writeUByte(value: UByte, offset: ULong = 0uL)
+    fun readUByte(offset: ULong = 0uL): UByte
+
+    fun writeChar(value: Char, offset: ULong = 0uL)
+    fun readChar(offset: ULong = 0uL): Char
+
+    fun writeShort(value: Short, offset: ULong = 0uL)
+    fun readShort(offset: ULong = 0uL): Short
+
+    fun writeUShort(value: UShort, offset: ULong = 0uL)
+    fun readUShort(offset: ULong = 0uL): UShort
+
+    fun writeInt(value: Int, offset: ULong = 0uL)
+    fun readInt(offset: ULong = 0uL): Int
+
+    fun writeUInt(value: UInt, offset: ULong = 0uL)
+    fun readUInt(offset: ULong = 0uL): UInt
+
+    fun writeLong(value: Long, offset: ULong = 0uL)
+    fun readLong(offset: ULong = 0uL): Long
+
+    fun writeULong(value: ULong, offset: ULong = 0uL)
+    fun readULong(offset: ULong = 0uL): ULong
+
+    fun writeFloat(value: Float, offset: ULong = 0uL)
+    fun readFloat(offset: ULong = 0uL): Float
+
+    fun writeDouble(value: Double, offset: ULong = 0uL)
+    fun readDouble(offset: ULong = 0uL): Double
+
+    fun writePointer(value: NativeAddress, offset: ULong = 0uL)
+    fun readPointer(offset: ULong = 0uL): NativeAddress
+
+    fun writeString(value: String, offset: ULong = 0uL)
+    fun readString(offset: ULong = 0uL): String
+
+    fun writeBytes(array: ByteArray, offset: ULong = 0uL, size: ULong = array.size.toULong())
+    fun readBytes(array: ByteArray, offset: ULong = 0uL, size: ULong = array.size.toULong())
+
+    fun writeUBytes(array: UByteArray, offset: ULong = 0uL, size: ULong = array.size.toULong())
+    fun readYBytes(array: UByteArray, offset: ULong = 0uL, size: ULong = array.size.toULong())
+
+    fun writeShorts(array: ShortArray, offset: ULong = 0uL, size: ULong = array.size.toULong())
+    fun readShorts(array: ShortArray, offset: ULong = 0uL, size: ULong)
+
+    fun writeUShorts(array: UShortArray, offset: ULong = 0uL, size: ULong = array.size.toULong())
+    fun readUShorts(array: UShortArray, offset: ULong = 0uL, size: ULong)
+
+    fun writeChars(array: CharArray, offset: ULong = 0uL, size: ULong = array.size.toULong())
+    fun readChars(array: CharArray, offset: ULong = 0uL, size: ULong)
+
+    fun writeInts(array: IntArray, offset: ULong = 0uL, size: ULong = array.size.toULong())
+    fun readInts(array: IntArray, offset: ULong = 0uL, size: ULong)
+
+    fun writeUInts(array: UIntArray, offset: ULong = 0uL, size: ULong = array.size.toULong())
+    fun readUInts(array: UIntArray, offset: ULong = 0uL, size: ULong)
+
+    fun writeLongs(array: LongArray, offset: ULong = 0uL, size: ULong = array.size.toULong())
+    fun readLongs(array: LongArray, offset: ULong = 0uL, size: ULong)
+
+    fun writeULongs(array: ULongArray, offset: ULong = 0uL, size: ULong = array.size.toULong())
+    fun readULongs(array: ULongArray, offset: ULong = 0uL, size: ULong)
+
+    fun writeFloats(array: FloatArray, offset: ULong = 0uL, size: ULong = array.size.toULong())
+    fun readFloats(array: FloatArray, offset: ULong = 0uL, size: ULong)
+
+    fun writeDoubles(array: DoubleArray, offset: ULong = 0uL, size: ULong = array.size.toULong())
+    fun readDoubles(array: DoubleArray, offset: ULong = 0uL, size: ULong)
 }
