9268ac2baa94baee8bd2db84494801ad50cfd29e
ygdrasil-io <alexandre.mommers@gmail.com>
2024-10-21 00:45:25 +0200

    Add extensive read/write methods to MemoryBuffer class
    Implemented various data type read and write methods in the MemoryBuffer class for both Android and native platforms. This change significantly enhances the buffer's capability to handle different types of data efficiently.

--- wgpu4k-native/src/androidMain/kotlin/ffi/Buffer.android.kt
+++ wgpu4k-native/src/androidMain/kotlin/ffi/Buffer.android.kt
@@ -1,32 +1,267 @@
 package ffi
 
 actual class MemoryBuffer actual constructor(actual val handler: NativeAddress, actual val size: ULong) {
-    actual fun readLong(): Long {
-        return handler.getLong(0)
+
+    actual fun writeByte(value: Byte, offset: ULong) {
+        handler.setByte(offset.toLong(), value)
     }
 
-    actual fun writeLong(value: Long) {
+    actual fun readByte(offset: ULong): Byte {
+        return handler.getByte(offset.toLong())
     }
 
-    actual fun readFloat(): Float {
+    actual fun writeUByte(value: UByte, offset: ULong) {
+    }
+
+    actual fun readUByte(offset: ULong): UByte {
         TODO("Not yet implemented")
     }
 
-    actual fun writeFloat(value: Float) {
+    actual fun writeChar(value: Char, offset: ULong) {
     }
 
-    actual fun readInt(): Int {
-        return handler.getInt(0)
-    }
-
-    actual fun writeInt(value: Int) {
-    }
-
-    actual fun readPointer(): NativeAddress {
+    actual fun readChar(offset: ULong): Char {
         TODO("Not yet implemented")
     }
 
-    actual fun writePointer(value: NativeAddress) {
+    actual fun writeShort(value: Short, offset: ULong) {
     }
+
+    actual fun readShort(offset: ULong): Short {
+        TODO("Not yet implemented")
+    }
+
+    actual fun writeUShort(value: UShort, offset: ULong) {
+    }
+
+    actual fun readUShort(offset: ULong): UShort {
+        TODO("Not yet implemented")
+    }
+
+    actual fun writeInt(value: Int, offset: ULong) {
+    }
+
+    actual fun readInt(offset: ULong): Int {
+        TODO("Not yet implemented")
+    }
+
+    actual fun writeUInt(value: UInt, offset: ULong) {
+    }
+
+    actual fun readUInt(offset: ULong): UInt {
+        TODO("Not yet implemented")
+    }
+
+    actual fun writeLong(value: Long, offset: ULong) {
+    }
+
+    actual fun readLong(offset: ULong): Long {
+        TODO("Not yet implemented")
+    }
+
+    actual fun writeULong(value: ULong, offset: ULong) {
+    }
+
+    actual fun readULong(offset: ULong): ULong {
+        TODO("Not yet implemented")
+    }
+
+    actual fun writeFloat(value: Float, offset: ULong) {
+    }
+
+    actual fun readFloat(offset: ULong): Float {
+        TODO("Not yet implemented")
+    }
+
+    actual fun writeDouble(value: Double, offset: ULong) {
+    }
+
+    actual fun readDouble(offset: ULong): Double {
+        TODO("Not yet implemented")
+    }
+
+    actual fun writePointer(value: NativeAddress, offset: ULong) {
+    }
+
+    actual fun readPointer(offset: ULong): NativeAddress {
+        TODO("Not yet implemented")
+    }
+
+    actual fun writeBytes(
+        array: ByteArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun readBytes(
+        array: ByteArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun writeUBytes(
+        array: UByteArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun readUBytes(
+        array: UByteArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun writeShorts(
+        array: ShortArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun readShorts(
+        array: ShortArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun writeUShorts(
+        array: UShortArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun readUShorts(
+        array: UShortArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun writeChars(
+        array: CharArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun readChars(
+        array: CharArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun writeInts(
+        array: IntArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun readInts(
+        array: IntArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun writeUInts(
+        array: UIntArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun readUInts(
+        array: UIntArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun writeLongs(
+        array: LongArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun readLongs(
+        array: LongArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun writeULongs(
+        array: ULongArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun readULongs(
+        array: ULongArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun writeFloats(
+        array: FloatArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun readFloats(
+        array: FloatArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun writeDoubles(
+        array: DoubleArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun readDoubles(
+        array: DoubleArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
 
 }
--- wgpu4k-native/src/nativeMain/kotlin/ffi/Buffer.native.kt
+++ wgpu4k-native/src/nativeMain/kotlin/ffi/Buffer.native.kt
@@ -10,32 +10,265 @@ import kotlinx.cinterop.toCPointer
 import kotlinx.cinterop.value
 
 actual class MemoryBuffer actual constructor(actual val handler: NativeAddress, actual val size: ULong) {
-    actual fun readLong(): Long {
+    actual fun writeByte(value: Byte, offset: ULong) {
+    }
+
+    actual fun readByte(offset: ULong): Byte {
         TODO("Not yet implemented")
     }
 
-    actual fun writeLong(value: Long) {
+    actual fun writeUByte(value: UByte, offset: ULong) {
     }
 
-    actual fun readFloat(): Float {
+    actual fun readUByte(offset: ULong): UByte {
         TODO("Not yet implemented")
     }
 
-    actual fun writeFloat(value: Float) {
+    actual fun writeChar(value: Char, offset: ULong) {
     }
 
-    actual fun readInt(): Int {
-        return handler.reinterpret<IntVar>().pointed.value
-    }
-
-    actual fun writeInt(value: Int) {
-    }
-
-    actual fun readPointer(): NativeAddress {
+    actual fun readChar(offset: ULong): Char {
         TODO("Not yet implemented")
     }
 
-    actual fun writePointer(value: NativeAddress) {
+    actual fun writeShort(value: Short, offset: ULong) {
     }
+
+    actual fun readShort(offset: ULong): Short {
+        TODO("Not yet implemented")
+    }
+
+    actual fun writeUShort(value: UShort, offset: ULong) {
+    }
+
+    actual fun readUShort(offset: ULong): UShort {
+        TODO("Not yet implemented")
+    }
+
+    actual fun writeInt(value: Int, offset: ULong) {
+    }
+
+    actual fun readInt(offset: ULong): Int {
+        TODO("Not yet implemented")
+    }
+
+    actual fun writeUInt(value: UInt, offset: ULong) {
+    }
+
+    actual fun readUInt(offset: ULong): UInt {
+        TODO("Not yet implemented")
+    }
+
+    actual fun writeLong(value: Long, offset: ULong) {
+    }
+
+    actual fun readLong(offset: ULong): Long {
+        TODO("Not yet implemented")
+    }
+
+    actual fun writeULong(value: ULong, offset: ULong) {
+    }
+
+    actual fun readULong(offset: ULong): ULong {
+        TODO("Not yet implemented")
+    }
+
+    actual fun writeFloat(value: Float, offset: ULong) {
+    }
+
+    actual fun readFloat(offset: ULong): Float {
+        TODO("Not yet implemented")
+    }
+
+    actual fun writeDouble(value: Double, offset: ULong) {
+    }
+
+    actual fun readDouble(offset: ULong): Double {
+        TODO("Not yet implemented")
+    }
+
+    actual fun writePointer(value: NativeAddress, offset: ULong) {
+    }
+
+    actual fun readPointer(offset: ULong): NativeAddress {
+        TODO("Not yet implemented")
+    }
+
+    actual fun writeBytes(
+        array: ByteArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun readBytes(
+        array: ByteArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun writeUBytes(
+        array: UByteArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun readUBytes(
+        array: UByteArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun writeShorts(
+        array: ShortArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun readShorts(
+        array: ShortArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun writeUShorts(
+        array: UShortArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun readUShorts(
+        array: UShortArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun writeChars(
+        array: CharArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun readChars(
+        array: CharArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun writeInts(
+        array: IntArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun readInts(
+        array: IntArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun writeUInts(
+        array: UIntArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun readUInts(
+        array: UIntArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun writeLongs(
+        array: LongArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun readLongs(
+        array: LongArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun writeULongs(
+        array: ULongArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun readULongs(
+        array: ULongArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun writeFloats(
+        array: FloatArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun readFloats(
+        array: FloatArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun writeDoubles(
+        array: DoubleArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
+    actual fun readDoubles(
+        array: DoubleArray,
+        arrayIndex: ULong,
+        bufferOffset: ULong,
+        size: ULong
+    ) {
+    }
+
 
 }
