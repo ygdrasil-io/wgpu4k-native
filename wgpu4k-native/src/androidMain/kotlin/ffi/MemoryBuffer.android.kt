4ff6a232e5e11610c0ca6337f23a4b21c66aea2a
ygdrasil-io <alexandre.mommers@gmail.com>
2024-12-18 00:09:54 +0100

    Implement memory buffer read/write operations for Android
    Replaced unimplemented methods with functional calls to the `handler` to enable proper reading and writing of various data types such as integers, floats, and pointers. This ensures the `MemoryBuffer` class now fully supports interaction with underlying memory on Android.

--- wgpu4k-native/src/androidMain/kotlin/ffi/MemoryBuffer.android.kt
+++ wgpu4k-native/src/androidMain/kotlin/ffi/MemoryBuffer.android.kt
@@ -13,73 +13,83 @@ actual class MemoryBuffer actual constructor(actual val handler: NativeAddress, 
     }
 
     actual fun writeUByte(value: UByte, offset: ULong) {
+        handler.setByte(offset.toLong(), value.toByte())
     }
 
     actual fun readUByte(offset: ULong): UByte {
-        TODO("Not yet implemented")
+        return handler.getByte(offset.toLong()).toUByte()
     }
 
     actual fun writeShort(value: Short, offset: ULong) {
+        handler.setShort(offset.toLong(), value)
     }
 
     actual fun readShort(offset: ULong): Short {
-        TODO("Not yet implemented")
+        return handler.getShort(offset.toLong())
     }
 
     actual fun writeUShort(value: UShort, offset: ULong) {
+        handler.setShort(offset.toLong(), value.toShort())
     }
 
     actual fun readUShort(offset: ULong): UShort {
-        TODO("Not yet implemented")
+        return handler.getShort(offset.toLong()).toUShort()
     }
 
     actual fun writeInt(value: Int, offset: ULong) {
+        handler.setInt(offset.toLong(), value)
     }
 
     actual fun readInt(offset: ULong): Int {
-        TODO("Not yet implemented")
+        return handler.getInt(offset.toLong())
     }
 
     actual fun writeUInt(value: UInt, offset: ULong) {
+        handler.setInt(offset.toLong(), value.toInt())
     }
 
     actual fun readUInt(offset: ULong): UInt {
-        TODO("Not yet implemented")
+        return handler.getInt(offset.toLong()).toUInt()
     }
 
     actual fun writeLong(value: Long, offset: ULong) {
+        handler.setLong(offset.toLong(), value)
     }
 
     actual fun readLong(offset: ULong): Long {
-        TODO("Not yet implemented")
+        return handler.getLong(offset.toLong())
     }
 
     actual fun writeULong(value: ULong, offset: ULong) {
+        handler.setLong(offset.toLong(), value.toLong())
     }
 
     actual fun readULong(offset: ULong): ULong {
-        TODO("Not yet implemented")
+        return handler.getLong(offset.toLong()).toULong()
     }
 
     actual fun writeFloat(value: Float, offset: ULong) {
+        handler.setFloat(offset.toLong(), value)
     }
 
     actual fun readFloat(offset: ULong): Float {
-        TODO("Not yet implemented")
+        return handler.getFloat(offset.toLong())
     }
 
     actual fun writeDouble(value: Double, offset: ULong) {
+        handler.setDouble(offset.toLong(), value)
     }
 
     actual fun readDouble(offset: ULong): Double {
-        TODO("Not yet implemented")
+        return handler.getDouble(offset.toLong())
     }
 
     actual fun writePointer(value: NativeAddress, offset: ULong) {
+        handler.setPointer(offset.toLong(), value)
     }
 
     actual fun readPointer(offset: ULong): NativeAddress {
-        TODO("Not yet implemented")
+        return handler.getPointer(offset.toLong())
     }
 
     actual fun writeBytes(
