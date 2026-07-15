6d544957dfeba6046d4ab04c4f9d2af408937ecc
ygdrasil-io <alexandre.mommers@gmail.com>
2026-07-15 19:02:59 +0200

    test: make common GPU cleanup tests Android-safe

--- demo/common/src/commonMain/kotlin/ext.kt
+++ demo/common/src/commonMain/kotlin/ext.kt
@@ -5,12 +5,10 @@ package io.ygdrasil.wgpu
 import io.ygdrasil.kffi.ArrayHolder
 import io.ygdrasil.kffi.CallbackPolicy
 import io.ygdrasil.kffi.CallbackRegistration
-import io.ygdrasil.kffi.MemoryAllocator
 import io.ygdrasil.kffi.NativeAddress
 import io.ygdrasil.kffi.memoryScope
 import kotlin.concurrent.atomics.AtomicInt
 
-val allocator = MemoryAllocator()
 private val logCallbackConfigurationLock = AtomicInt(0)
 private var logCallback: CallbackRegistration<WGPULogCallback>? = null
 
