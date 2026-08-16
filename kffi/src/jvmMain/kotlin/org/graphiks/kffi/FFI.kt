f6cdb8f0e803be28971ded6835dac9257da26885
ygdrasil-io <alexandre.mommers@gmail.com>
2026-08-16 13:17:43 +0200

    chore(kffi): drop residual FFM exposure from JVM public surface

--- kffi/src/jvmMain/kotlin/org/graphiks/kffi/FFI.kt
+++ kffi/src/jvmMain/kotlin/org/graphiks/kffi/FFI.kt
@@ -1,21 +1,10 @@
 package org.graphiks.kffi
 
-import java.lang.foreign.FunctionDescriptor
 import java.lang.foreign.Linker
 import java.lang.foreign.SymbolLookup
 import java.lang.foreign.ValueLayout
-import java.lang.invoke.MethodHandle
-import java.lang.invoke.MethodHandles
 
-val C_BOOL: ValueLayout = ValueLayout.JAVA_BOOLEAN
-val C_CHAR: ValueLayout = ValueLayout.JAVA_BYTE
-val C_SHORT: ValueLayout = ValueLayout.JAVA_SHORT
-val C_INT: ValueLayout = ValueLayout.JAVA_INT
-val C_LONG_LONG: ValueLayout = ValueLayout.JAVA_LONG
-val C_FLOAT: ValueLayout = ValueLayout.JAVA_FLOAT
-val C_DOUBLE: ValueLayout = ValueLayout.JAVA_DOUBLE
 val C_POINTER: ValueLayout = ValueLayout.ADDRESS
-val C_LONG: ValueLayout = ValueLayout.JAVA_LONG
 
 private val SYMBOL_LOOKUP by lazy {
     SymbolLookup.loaderLookup()
@@ -27,12 +16,4 @@ fun findOrThrow(symbol: String): Long {
     return SYMBOL_LOOKUP.find(symbol)
         .orElseThrow { UnsatisfiedLinkError("unresolved symbol: $symbol") }
         .address()
-}
-
-fun upcallHandle(fi: Class<*>?, name: String?, fdesc: FunctionDescriptor): MethodHandle {
-    try {
-        return MethodHandles.lookup().findVirtual(fi, name, fdesc.toMethodType())
-    } catch (ex: ReflectiveOperationException) {
-        throw AssertionError(ex)
-    }
 }
