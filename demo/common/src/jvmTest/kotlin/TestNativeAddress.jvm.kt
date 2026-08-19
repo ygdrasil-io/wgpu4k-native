f95d97875b277ed1a02d10366c6993395a2aab39
ygdrasil-io <alexandre.mommers@gmail.com>
2026-08-19 03:37:11 +0200

    fix(jvm): adapt NativeAddress test to Long API

--- demo/common/src/jvmTest/kotlin/TestNativeAddress.jvm.kt
+++ demo/common/src/jvmTest/kotlin/TestNativeAddress.jvm.kt
@@ -3,4 +3,4 @@ package io.ygdrasil.wgpu
 import org.graphiks.kffi.NativeAddress
 import java.lang.foreign.MemorySegment
 
-actual fun testNativeAddress(): NativeAddress = NativeAddress(MemorySegment.ofAddress(1L))
+actual fun testNativeAddress(): NativeAddress = NativeAddress(MemorySegment.ofAddress(1L).address())
